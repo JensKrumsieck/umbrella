404a9338f2f77cf3094306736922c8d4a36036ad
Jens Krumsieck <j.krumsieck@outlook.de>
2024-10-09 09:20:05 +0000

    remove try

--- src/index.py
+++ src/index.py
@@ -10,6 +10,7 @@ from pybadges import badge
 load_dotenv()
 app = FastAPI()
 
+
 @app.get("/")
 def index():
     html_content = """
@@ -25,68 +26,70 @@ def index():
         """
     return Response(html_content, media_type="text/html")
 
+
 @app.get("/coverage/{user}/{repo}")
 def read_coverage(user: str, repo: str, branch="main"):
-    try:
-        # get workflow runs
-        runs_url = f"https://api.github.com/repos/{user}/{repo}/actions/workflows/coverage.yml/runs?status=success&branch={branch}&per_page=1&event=push"
-        response = requests.get(runs_url)
-        if not response.ok:
-            raise HTTPException(status_code=response.status_code, detail=f"GitHub API returned error. Request URL: {runs_url}")
-        run_json = response.json()
-        run_id = run_json["workflow_runs"][0]["id"]
+    # get workflow runs
+    runs_url = f"https://api.github.com/repos/{user}/{repo}/actions/workflows/coverage.yml/runs?status=success&branch={branch}&per_page=1&event=push"
+    response = requests.get(runs_url)
+    if not response.ok:
+        raise HTTPException(status_code=response.status_code,
+                            detail=f"GitHub API returned error. Request URL: {runs_url}")
+    run_json = response.json()
+    run_id = run_json["workflow_runs"][0]["id"]
 
-        # list artifacts
-        artifacts_url = f"https://api.github.com/repos/{user}/{repo}/actions/runs/{run_id}/artifacts?per_page=100"
-        response = requests.get(artifacts_url)
-        if not response.ok:
-            raise HTTPException(status_code=response.status_code, detail=f"GitHub API returned error. Request URL: {artifacts_url}")
-        artifacts_json = response.json()
+    # list artifacts
+    artifacts_url = f"https://api.github.com/repos/{user}/{repo}/actions/runs/{run_id}/artifacts?per_page=100"
+    response = requests.get(artifacts_url)
+    if not response.ok:
+        raise HTTPException(status_code=response.status_code,
+                            detail=f"GitHub API returned error. Request URL: {artifacts_url}")
+    artifacts_json = response.json()
 
-        # pick first artifact and download
-        zip_url = artifacts_json["artifacts"][0]["archive_download_url"]
-        headers = {'Authorization': 'token ' + os.environ.get("API_TOKEN")}
-        response = requests.get(zip_url, headers=headers, stream=True)
-        if not response.ok:
-            raise HTTPException(status_code=response.status_code, detail=f"GitHub API returned error. Request URL: {zip_url}")
-        
-        # Extract ZIP file from the response content
-        zip_file = BytesIO(response.content)
-        line_rate_float = 0
-        with zipfile.ZipFile(zip_file, 'r') as zip_ref:
-            xml_file_name = None
-            for file_info in zip_ref.infolist():
-                if file_info.filename.endswith('.xml'):
-                    xml_file_name = file_info.filename
-                    break
+    # pick first artifact and download
+    zip_url = artifacts_json["artifacts"][0]["archive_download_url"]
+    headers = {'Authorization': 'token ' + os.environ.get("API_TOKEN")}
+    response = requests.get(zip_url, headers=headers, stream=True)
+    if not response.ok:
+        raise HTTPException(status_code=response.status_code,
+                            detail=f"GitHub API returned error. Request URL: {zip_url}")
 
-            if xml_file_name:
-                with zip_ref.open(xml_file_name) as xml_file:
-                    tree = ET.parse(xml_file)
-                    root = tree.getroot()
-                    line_rate = root.attrib.get('line-rate')
-                    line_rate_float = float(line_rate)
-            else:
-                raise HTTPException(status_code=500, detail="could not find the required xml file...")
-            
-        color = 'green'
+    # Extract ZIP file from the response content
+    zip_file = BytesIO(response.content)
+    line_rate_float = 0
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        xml_file_name = None
+        for file_info in zip_ref.infolist():
+            if file_info.filename.endswith('.xml'):
+                xml_file_name = file_info.filename
+                break
 
-        if line_rate_float < 75:
-            color = 'yellow'
+        if xml_file_name:
+            with zip_ref.open(xml_file_name) as xml_file:
+                tree = ET.parse(xml_file)
+                root = tree.getroot()
+                line_rate = root.attrib.get('line-rate')
+                line_rate_float = float(line_rate)
+        else:
+            raise HTTPException(
+                status_code=500, detail="could not find the required xml file...")
 
-        if line_rate_float < 50:
-            color = 'red'
+    color = 'green'
 
-        if line_rate_float < 25:
-            color = '#8b0000'
+    if line_rate_float < 75:
+        color = 'yellow'
 
-        # create badge
-        cov = badge(left_text="coverage",
-                    right_text=f"{round(line_rate_float, 2)} %", right_color=color)
-        return Response(cov, media_type="image/svg+xml")
-    
-    except:
-        return HTTPException(status_code=500, detail="🚨 Oh no! I could not proceed on your request...")
+    if line_rate_float < 50:
+        color = 'red'
+
+    if line_rate_float < 25:
+        color = '#8b0000'
+
+    # create badge
+    cov = badge(left_text="coverage",
+                right_text=f"{round(line_rate_float, 2)} %", right_color=color)
+    return Response(cov, media_type="image/svg+xml")
+
 
 if __name__ == '__main__':
     import uvicorn
