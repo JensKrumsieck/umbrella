51aa9a09e63a2bdbc2d10a2ccf03a149b8822da5
Jens Krumsieck <j.krumsieck@outlook.de>
2024-10-10 07:33:38 +0000

    refactor

---
+++ src/coverage.py
@@ -0,0 +1,79 @@
+from io import BytesIO
+import zipfile
+from fastapi import HTTPException
+import xml.etree.ElementTree as ET
+from pybadges import badge
+import requests
+
+from src.util import evaluate_color
+
+
+def get_run_id(user: str, repo: str, branch: str, filename: str, headers: any) -> str:
+    # get workflow runs
+    runs_url = f"https://api.github.com/repos/{user}/{repo}/actions/workflows/{filename}/runs?status=success&branch={branch}&per_page=1&event=push"
+    response = requests.get(runs_url, headers=headers)
+    if not response.ok:
+        raise HTTPException(status_code=response.status_code,
+                            detail=f"GitHub API returned error. Request URL: {runs_url}")
+    run_json = response.json()
+    if len(run_json["workflow_runs"]) == 0:
+        raise HTTPException(
+            status_code=404, detail=f"Could not find any Workflow runs for {runs_url}")
+
+    return run_json["workflow_runs"][0]["id"]
+
+
+def get_artifact_url(user: str, repo: str, run_id: str, headers: any) -> str:
+    # list artifacts
+    artifacts_url = f"https://api.github.com/repos/{user}/{repo}/actions/runs/{run_id}/artifacts?per_page=100"
+    response = requests.get(artifacts_url, headers=headers)
+    if not response.ok:
+        raise HTTPException(status_code=response.status_code,
+                            detail=f"GitHub API returned error. Request URL: {artifacts_url}")
+    artifacts_json = response.json()
+
+    # pick first artifact and download
+    if len(artifacts_json["artifacts"][0]) == 0:
+        raise HTTPException(
+            status_code=404, detail=f"Could not find any artifacts")
+
+    return artifacts_json["artifacts"][0]["archive_download_url"]
+
+
+def download_zip_file(zip_url: str, headers: any) -> BytesIO:
+    response = requests.get(zip_url, headers=headers, stream=True)
+    if not response.ok:
+        raise HTTPException(status_code=response.status_code,
+                            detail=f"GitHub API returned error. Request URL: {zip_url}")
+
+    # Extract ZIP file from the response content
+    return BytesIO(response.content)
+
+
+def cobertura_get_line_rate(zip_file: BytesIO) -> float:
+    line_rate_float = 0
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        xml_file_name = None
+        for file_info in zip_ref.infolist():
+            if file_info.filename.endswith('.xml'):
+                xml_file_name = file_info.filename
+                break
+
+        if xml_file_name:
+            with zip_ref.open(xml_file_name) as xml_file:
+                tree = ET.parse(xml_file)
+                root = tree.getroot()
+                line_rate = root.attrib.get('line-rate')
+                line_rate_float = float(line_rate) * 100
+        else:
+            raise HTTPException(
+                status_code=500, detail="Required XML coverage report not found in the artifact ZIP.")
+    return line_rate_float
+
+
+def create_badge(value: float):
+    color = evaluate_color(value)
+
+    # create badge
+    return badge(left_text="coverage",
+                right_text=f"{round(value, 2)} %", right_color=color)
--- src/index.py
+++ src/index.py
@@ -1,95 +1,35 @@
-from io import BytesIO
 import os
-import zipfile
-from fastapi import FastAPI, HTTPException, Response
+from fastapi import FastAPI, Request, Response
 from dotenv import load_dotenv
-import requests
-import xml.etree.ElementTree as ET
-from pybadges import badge
-from .util import evaluate_color
+from fastapi.responses import HTMLResponse
+
+from .coverage import cobertura_get_line_rate, create_badge, get_artifact_url, download_zip_file, get_run_id
 
 load_dotenv()
 app = FastAPI()
 
 
-@app.get("/")
-def index():
-    html_content = """
-        <html>
-            <head>
-                <title>FastAPI HTML Response</title>
-            </head>
-            <body>
-                <h1>Coverage Service</h1>
-                <p>You probably want to see docs: <a href=\"/docs\">Docs</a></p>
-            </body>
-        </html>
-        """
-    return Response(html_content, media_type="text/html")
-
-
 @app.get("/coverage/{user}/{repo}")
 def read_coverage(user: str, repo: str, branch="main"):
     headers = {'Authorization': 'token ' + os.environ.get("API_TOKEN")}
-    # get workflow runs
-    runs_url = f"https://api.github.com/repos/{user}/{repo}/actions/workflows/coverage.yml/runs?status=success&branch={branch}&per_page=1&event=push"
-    response = requests.get(runs_url, headers=headers)
-    if not response.ok:
-        raise HTTPException(status_code=response.status_code,
-                            detail=f"GitHub API returned error. Request URL: {runs_url}")
-    run_json = response.json()
-    if len(run_json["workflow_runs"]) == 0:
-        raise HTTPException(
-            status_code=404, detail=f"Could not find any Workflow runs for {runs_url}")
 
-    run_id = run_json["workflow_runs"][0]["id"]
+    run_id = get_run_id(user, repo, branch, "coverage.yml", headers)
 
-    # list artifacts
-    artifacts_url = f"https://api.github.com/repos/{user}/{repo}/actions/runs/{run_id}/artifacts?per_page=100"
-    response = requests.get(artifacts_url, headers=headers)
-    if not response.ok:
-        raise HTTPException(status_code=response.status_code,
-                            detail=f"GitHub API returned error. Request URL: {artifacts_url}")
-    artifacts_json = response.json()
+    zip_url = get_artifact_url(user, repo, run_id, headers)
 
-    # pick first artifact and download
-    if len(artifacts_json["artifacts"][0]) == 0:
-        raise HTTPException(
-            status_code=404, detail=f"Could not find any artifacts")
+    zip_file = download_zip_file(zip_url, headers)
 
-    zip_url = artifacts_json["artifacts"][0]["archive_download_url"]
-    response = requests.get(zip_url, headers=headers, stream=True)
-    if not response.ok:
-        raise HTTPException(status_code=response.status_code,
-                            detail=f"GitHub API returned error. Request URL: {zip_url}")
+    line_rate_float = cobertura_get_line_rate(zip_file)
 
-    # Extract ZIP file from the response content
-    zip_file = BytesIO(response.content)
-    line_rate_float = 0
-    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
-        xml_file_name = None
-        for file_info in zip_ref.infolist():
-            if file_info.filename.endswith('.xml'):
-                xml_file_name = file_info.filename
-                break
+    badge = create_badge(line_rate_float)
 
-        if xml_file_name:
-            with zip_ref.open(xml_file_name) as xml_file:
-                tree = ET.parse(xml_file)
-                root = tree.getroot()
-                line_rate = root.attrib.get('line-rate')
-                line_rate_float = float(line_rate) * 100
-        else:
-            raise HTTPException(
-                status_code=500, detail="could not find the required xml file...")
+    return Response(badge, media_type="image/svg+xml")
 
-    color = evaluate_color(line_rate_float)
 
-    # create badge
-    cov = badge(left_text="coverage",
-                right_text=f"{round(line_rate_float, 2)} %", right_color=color)
-    return Response(cov, media_type="image/svg+xml")
-
+@app.get("/{full_path:path}")
+async def catch_all(request: Request):
+    with open("static/index.html") as f:
+        return HTMLResponse(f.read(), status_code=200)
 
 if __name__ == '__main__':
     import uvicorn
